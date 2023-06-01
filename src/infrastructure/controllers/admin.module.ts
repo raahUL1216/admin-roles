5e2bd3a723b7d22906f3e2b53c2be1ab30081323
Rahul Chodvadiya <rahulchodvadiya@gmail.com>
2023-06-01 22:43:32 +0530

    add test API

--- src/infrastructure/controllers/admin.module.ts
+++ src/infrastructure/controllers/admin.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common";
 import { UsecasesProxyModule } from "../usecases-proxy/usecases-proxy.module";
 import { AuthController } from "./auth/auth.controller";
 import { AdminController } from "./admin/admin.controller";
+import { AppController } from "./app.controller";
 
 @Module({
   imports: [UsecasesProxyModule.register()],
-  controllers: [AuthController, AdminController],
+  controllers: [AppController, AuthController, AdminController],
 })
 export class AdminModule {}
